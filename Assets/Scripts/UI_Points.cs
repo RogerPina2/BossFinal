2d39d1bc9334ffc777305ba1432623349ad0baee
Roger Pina <rogerrfp@al.insper.edu.br>
2021-05-27 18:15:40 -0300

    GameManager v0

---
+++ Assets/Scripts/UI_Points.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Points : MonoBehaviour
+{
+    Text textComp;
+    GameManager gm;
+    void Start()
+    {
+        textComp = GetComponent<Text>();
+        gm = GameManager.GetInstance();
+    }
+
+    void Update()
+    {
+        textComp.text = $"Pontos: {gm.points}";
+    }
+}
